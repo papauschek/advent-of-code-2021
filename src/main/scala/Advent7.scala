75708942624797136253bb255fa287c72c2236d3
Christian Papauschek <christian@journiapp.com>
2021-12-07 09:46:40 +0100

    shorter

--- src/main/scala/Advent7.scala
+++ src/main/scala/Advent7.scala
@@ -5,10 +5,8 @@ object Advent7 {
   def run(): Unit = {
     val file = Files.readString(Path.of("./data/input7.txt"))
     val positions = file.split(',').toList.map(_.toInt)
-    val minPosition = (0 until positions.max).minBy {
-      target => getFuelCost(positions, target)
-    }
-    println(getFuelCost(positions, minPosition))
+    val minPosition = (positions.sum / positions.length.toDouble).toInt
+    println(getFuelCost(positions, minPosition)) // 95581659
   }
 
   private def getFuelCost(positions: Seq[Int], targetPosition: Int): Int = {
