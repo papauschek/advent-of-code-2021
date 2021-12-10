bee9c1f43235f8844496862d41a6e99f171c90ad
Christian Papauschek <christian@journiapp.com>
2021-12-10 13:51:45 +0100

    advent 10 - 2

--- src/main/scala/Advent10.scala
+++ src/main/scala/Advent10.scala
@@ -5,8 +5,9 @@ object Advent10 {
 
   def run(): Unit = {
     val lines = Files.readString(Path.of("./data/input10.txt")).linesIterator.toList
-    val scores = lines.map(line => scoreLine(line))
-    println(scores.sum) // 366027
+    val scores = lines.map(line => scoreLine(line)).filter(_ > 0)
+    val median = scores.sorted.apply(scores.length / 2)
+    println(median) // 1118645287
   }
 
   val brackets: Map[Char, Char] = Map(
@@ -17,28 +18,24 @@ object Advent10 {
   )
 
   val score: Map[Char, Int] = Map(
-    ')' -> 3,
-    ']' -> 57,
-    '}' -> 1197,
-    '>' -> 25137
+    ')' -> 1,
+    ']' -> 2,
+    '}' -> 3,
+    '>' -> 4
   )
 
   @tailrec
-  private def scoreLine(line: String, openBrackets: List[Char] = Nil): Int = {
+  private def scoreLine(line: String, openBrackets: List[Char] = Nil): Long = {
     if (line.isEmpty) {
-      0
+      openBrackets.foldLeft(0L)((total, char) => total * 5 + score(char))
     } else {
       val char = line.head
       if (brackets.contains(char)) {
         scoreLine(line.tail, brackets(char) :: openBrackets)
       } else {
         openBrackets.headOption match {
-          case Some(closeChar) =>
-            if (closeChar == char) {
-              scoreLine(line.tail, openBrackets.tail)
-            } else {
-              score(char)
-            }
+          case Some(closeChar) if closeChar == char =>
+            scoreLine(line.tail, openBrackets.tail)
           case _ => 0
         }
       }
